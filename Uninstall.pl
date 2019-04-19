eb8a27118457bc3fa7ca603f0b5540f5816f0fbb
clallavena <clement.allavena@uca.fr>
2019-04-19 16:51:39 +0200

    Màj du fichier Uninstall

--- Uninstall.pl
+++ Uninstall.pl
@@ -9,22 +9,21 @@ open(my $out, ">", "/tmp/path.txt") or die "Can't open for output path.txt: $!";
 open(my $in, "<", "/tmp/path.txt") or die "Can't open for input path.txt: $!";
 
 my @files_name=("prometheus", "alertmanager", "node_exporter", "prometheus-files");
-print $files_name[$#files_name];
-print "\n";
 
 foreach my $line ( @files_name ){
 	print $out `sudo find / -name $line`;
-	system "find /etc/systemd/system/ -name $line.service | grep $line.service";
-	unless ( $? >= 1 ){
-		system "sudo systemctl disable $line.service";
-		system "sudo systemctl stop $line.service";
-		system "sudo rm -rf /etc/systemd/system/$line.service";
-	}	
+  my @list = `find /etc/systemd/system/ -name $line.service | grep $line.service`;
+	foreach my $file ( @list ){
+		print $file;
+		system "sudo systemctl disable $line.service && sudo systemctl stop $line.service && sudo rm -rf /etc/systemd/system/$line.service";
+	} 
 }
 
 foreach my $line ( <$in> ) {
 	system "sudo rm -rf $line";
 }
+
+
 
 close $out or die "$out: $!";
 close $in or die "$in: $!";
